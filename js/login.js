30140458d40f9b497baa6bc33b65f6be06446d51
sho-van <shovanbiswas.bd@gmail.com>
2023-04-03 10:35:09 +0600

    home page redirect done

--- js/login.js
+++ js/login.js
@@ -1,7 +1,14 @@
 
+// admin id and password
+const adminEmail = 'email@mail.com'
+const adminPassword = 'pass'
+
+//login button click event
 document.getElementById('login-button').addEventListener('click', function () {
     const loginEmail = document.getElementById('login-email').value
     const loginPassword = document.getElementById('login-password').value
-    console.log(loginEmail)
-    console.log(loginPassword)
+
+    if (loginEmail === adminEmail && loginPassword === adminPassword) {
+        window.location.href = 'home.html'
+    }
 })
