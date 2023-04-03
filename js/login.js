d6c694289a95854ccbec054ba6581eed363da2ee
sho-van <shovanbiswas.bd@gmail.com>
2023-04-03 10:21:37 +0600

    login page done

---
+++ js/login.js
@@ -0,0 +1,7 @@
+
+document.getElementById('login-button').addEventListener('click', function () {
+    const loginEmail = document.getElementById('login-email').value
+    const loginPassword = document.getElementById('login-password').value
+    console.log(loginEmail)
+    console.log(loginPassword)
+})
